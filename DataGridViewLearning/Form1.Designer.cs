68bc2c693a6038d37715581ea81824d514f9cb0d
landbroken <445299258@qq.com>
2017-11-25 17:16:36 +0800

    Update

--- DataGridViewLearning/Form1.Designer.cs
+++ DataGridViewLearning/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.Longtitude = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Latitude = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Velocity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -83,11 +84,22 @@
             this.Velocity.HeaderText = "Velocity";
             this.Velocity.Name = "Velocity";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(940, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1027, 428);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
@@ -107,6 +119,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Longtitude;
         private System.Windows.Forms.DataGridViewTextBoxColumn Latitude;
         private System.Windows.Forms.DataGridViewTextBoxColumn Velocity;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DataGridViewLearning/Form1.cs
+++ DataGridViewLearning/Form1.cs
@@ -19,16 +19,40 @@ namespace DataGridViewLearning
 
         private void button1_Click(object sender, EventArgs e)
         {
+            GetCurrentCell();
+            SetCell(1, 2, "new value");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SelectNextColumnCell(0);
+        }
+
+        private void GetCurrentCell()
+        {
             string strOut = "";
             try
             {
-                dataGridView1.CurrentCell = dataGridView1[1, 1];
                 int curColumn = dataGridView1.CurrentCell.ColumnIndex;
                 int curRow = dataGridView1.CurrentCell.RowIndex;
                 strOut += "curColumn: " + curColumn.ToString() + Environment.NewLine;
                 strOut += "curRow: " + curRow.ToString() + Environment.NewLine;
                 strOut += "dataGridView1.CurrentCell.Value: " + (string)dataGridView1.CurrentCell.Value + Environment.NewLine;
-                dataGridView1.CurrentCell.Value = "new value";
+            }
+            catch (Exception ex)
+            {
+                string strErr = ex.Message;
+            }
+            textBox1.Text = strOut;
+        }
+
+        private void SetCell(int Column,int Row,object NewValue)
+        {
+            string strOut = "";
+            try
+            {
+                dataGridView1.CurrentCell = dataGridView1[Column, Row];
+                dataGridView1.CurrentCell.Value = NewValue;
             }
             catch (ArgumentOutOfRangeException ex)
             {
@@ -36,11 +60,23 @@ namespace DataGridViewLearning
                 string strErr = ex.Message;
                 MessageBox.Show("No such cell,out of range!");
             }
-            catch (Exception ex)
-            {
-                string strErr = ex.Message;
-            }
             textBox1.Text = strOut;
+        }
+
+        private void SelectNextRowCell(int Column)
+        {
+            int row = this.dataGridView1.CurrentRow.Index + 1;
+            if (row > this.dataGridView1.RowCount - 1)
+                row = 0;
+            this.dataGridView1.CurrentCell = this.dataGridView1[Column, row];
+        }
+
+        private void SelectNextColumnCell(int Row)
+        {
+            int column = this.dataGridView1.CurrentCellAddress.X + 1;
+            if (column > this.dataGridView1.ColumnCount - 1)
+                column = 0;
+            this.dataGridView1.CurrentCell = this.dataGridView1[column, Row];
         }
     }
 }
